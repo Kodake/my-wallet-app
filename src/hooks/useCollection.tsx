4e041849a710ec3f37c55b466203ea7e4ffbe1df
Kodake <kodake@hotmail.es>
2022-04-09 03:29:57 -0400

    Adding query to useCollection hook

--- src/hooks/useCollection.tsx
+++ src/hooks/useCollection.tsx
@@ -1,17 +1,25 @@
-import { useReducer, useEffect, useState } from 'react';
-import { projectFirestore, timestamp } from '../firebase/config';
-import { Transaction, Transactions } from '../interfaces/appInterfaces';
+import { useEffect, useState, useRef } from 'react';
+import { projectFirestore } from '../firebase/config';
+import { Transaction } from '../interfaces/appInterfaces';
 
-export const useCollection = (collection: string) => {
+export const useCollection = (collection: string, _query: [string, string, string]) => {
     const [documents, setDocuments] = useState<Transaction[] | null>(null);
     const [error, setError] = useState<string | null>(null);
+
+    // if we don't use a ref --> infinite loop in useEffect
+    // _query is an array and is 'different' on every function call
+    const query = useRef(_query).current;
 
     useEffect(() => {
         // Get the collection ref
         // let ref = projectFirestore.collection(collection);
 
         // Get the collection ref using query
-        let ref = projectFirestore.collection(collection);
+        let ref = projectFirestore.collection(collection).where(...query);
+
+        if (query) {
+            ref = ref.where(...query);
+        }
 
         const unsubscribe = ref.onSnapshot((snapshot: any) => {
             let results: Transaction[] = [];
@@ -33,7 +41,7 @@ export const useCollection = (collection: string) => {
             unsubscribe();
         }
 
-    }, [collection]);
+    }, [collection, query]);
 
     return { documents, error };
 
